29176c34a421bb924e0d9432d85316ab630f5b9c
evanorti <87997759+evanorti@users.noreply.github.com>
2025-04-16 15:47:47 -0400

    Update lib.rs

--- components/eth-price-oracle/src/lib.rs
+++ components/eth-price-oracle/src/lib.rs
@@ -1,5 +1,39 @@
-mod trigger;
-use trigger::{decode_trigger_event, encode_trigger_output, Destination};
+use crate::bindings::wavs::worker::layer_types::{TriggerData, TriggerDataEthContractEvent};
+use alloy_sol_types::SolValue;
+use anyhow::Result;
+use wavs_wasi_chain::decode_event_log_data;
+
+pub enum Destination {
+    Ethereum,
+    CliOutput,
+}
+
+pub fn decode_trigger_event(trigger_data: TriggerData) -> Result<(u64, Vec<u8>, Destination)> {
+    match trigger_data {
+        TriggerData::EthContractEvent(TriggerDataEthContractEvent { log, .. }) => {
+            let event: solidity::NewTrigger = decode_event_log_data!(log)?;
+            let trigger_info = solidity::TriggerInfo::abi_decode(&event._triggerInfo, false)?;
+            Ok((trigger_info.triggerId, trigger_info.data.to_vec(), Destination::Ethereum))
+        }
+        TriggerData::Raw(data) => Ok((0, data.clone(), Destination::CliOutput)),
+        _ => Err(anyhow::anyhow!("Unsupported trigger data type")),
+    }
+}
+
+pub fn encode_trigger_output(trigger_id: u64, output: impl AsRef<[u8]>) -> Vec<u8> {
+    solidity::DataWithId { triggerId: trigger_id, data: output.as_ref().to_vec().into() }
+        .abi_encode()
+}
+
+mod solidity {
+    use alloy_sol_macro::sol;
+    pub use ITypes::*;
+
+    sol!("../../src/interfaces/ITypes.sol");
+}
+
+// --- End trigger.rs logic ---
+
 use wavs_wasi_chain::http::{fetch_json, http_request_get};
 pub mod bindings;
 use crate::bindings::{export, Guest, TriggerAction};
@@ -73,7 +107,6 @@ pub struct PriceFeedData {
 /// <https://transform.tools/json-to-rust-serde>
 /// Generated from <https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail?id=1&range=1h>
 /// -----
-///
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Root {
     pub data: Data,

6beaf95eff8ac999d65fe6774156bac372c3357f
evanorti <87997759+evanorti@users.noreply.github.com>
2025-04-29 18:19:13 -0400

    Update Trigger.s.sol

--- script/Trigger.s.sol
+++ script/Trigger.s.sol
@@ -8,11 +8,16 @@ import {console} from "forge-std/console.sol";
 
 /// @dev Script to add a new trigger
 contract Trigger is Common {
-    function run(string calldata serviceTriggerAddr, string calldata coinMarketCapID) public {
+    function run(
+        string calldata serviceTriggerAddr,
+        string calldata coinMarketCapID
+    ) public {
         vm.startBroadcast(_privateKey);
-        SimpleTrigger trigger = SimpleTrigger(vm.parseAddress(serviceTriggerAddr));
+        SimpleTrigger trigger = SimpleTrigger(
+            vm.parseAddress(serviceTriggerAddr)
+        );
 
-        trigger.addTrigger(abi.encodePacked(coinMarketCapID));
+        trigger.addTrigger(abi.encode(coinMarketCapID));
         ITypes.TriggerId triggerId = trigger.nextTriggerId();
         console.log("TriggerId", ITypes.TriggerId.unwrap(triggerId));
         vm.stopBroadcast();
